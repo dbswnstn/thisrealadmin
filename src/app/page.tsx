58628c4d564b197a257a80d45f5142f9de3dcd87
JUNSoo <dbswnstn6@gmail.com>
2025-11-02 23:34:14 +0900

    코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,34 @@
 
 'use client'
-import React, { useState , useEffect} from 'react';
 
-import { Button, Flex, Table } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
-
-type TableRowSelection<T extends object = object> = TableProps<T>['rowSelection'];
-
-interface DataType {
-  key: React.Key;
-  name: string;
-  age: number;
-  address: string;
-}
-
-const columns: TableColumnsType<DataType> = [
-  { title: 'Name', dataIndex: 'name' , width: '20%'},
-  { title: 'Age', dataIndex: 'age', width: '20%'},
-  { title: 'Address', dataIndex: 'address',width:'10%' },
-];
-
-const dataSource = Array.from<DataType>({ length: 46 }).map<DataType>((_, i) => ({
-  key: i,
-  name: `Edward King ${i}`,
-  age: 32,
-  address: `London, Park Lane no. ${i}`,
-}));
+import { useState , useEffect} from 'react';
+import { Flex } from 'antd';
 
 export default function Home() {
- 
+
+  const [userCount, setUserCount] = useState();
+
   useEffect(()=> {
     fetch('/api/total')
       .then(res => res.json())
       .then(data =>  {
-          console.log("ssss222dd", data) 
+         setUserCount(data)
+         // console.log("ssss222dd", data) 
         });
 
   }, []);
   
-  
-
   return (
-    
     <Flex gap="middle" vertical>
-      <div>
-        
-      </div>
+      {
+        userCount && <>
+            <div style={{fontSize: 18, fontWeight: 'bold'}}>총 가입자 수</div>
+            <div style={{fontSize: 18, display: 'flex', gap:20 }}> 
+              <span>{`남 : ${userCount?.manUser}`}</span>
+              <span>{`여 : ${userCount?.womanUser}`}</span>
+            </div>
+        </>
+      }
     </Flex>
   );
 }
